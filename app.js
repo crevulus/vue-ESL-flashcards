9d8de884312dd58890d4bc4cb5139ce70989fdbe
crevulus <crevulus@gmail.com>
2020-04-12 11:54:03 +0200

    template skeleton

---
+++ app.js
@@ -0,0 +1,27 @@
+const flashcards = [
+  {
+    definition: '(to) Be very busy working on [something].',
+    phrase: '(to) Be tied up with',
+    isFlipped: false,
+  },
+  {
+    definition: 'The number of people at an event.',
+    phrase: 'Turn-out',
+    isFlipped: false,
+  },
+  {
+    definition: '(to) Regain control of [something].',
+    phrase: '(to) Get back on top of',
+    isFlipped: false,
+  },
+]
+
+new Vue({
+  el: "#esl-flashcards",
+  data: {
+    flashcards: flashcards,
+  },
+  methods: {
+
+  },
+});

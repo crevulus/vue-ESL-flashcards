c171b657d35b9b860df7e566906e183fb25df86b
crevulus <crevulus@gmail.com>
2020-04-12 13:56:08 +0200

    Can't get single card marked as learned

--- app.js
+++ app.js
@@ -1,27 +1,51 @@
 const flashcards = [
   {
-    definition: '(to) Be very busy working on [something].',
     phrase: '(to) Be tied up with',
+    definition: '(to) Be very busy working on [something].',
     isFlipped: false,
+    isLearned: false,
   },
   {
-    definition: 'The number of people at an event.',
     phrase: 'Turn-out',
+    definition: 'The number of people at an event.',
     isFlipped: false,
+    isLearned: false,
   },
   {
-    definition: '(to) Regain control of [something].',
     phrase: '(to) Get back on top of',
+    definition: '(to) Regain control of [something].',
     isFlipped: false,
+    isLearned: false,
   },
-]
+];
 
 new Vue({
   el: "#esl-flashcards",
   data: {
     flashcards: flashcards,
+    inputPhrase: '',
+    inputDef: '',
+    isLearned: false,
   },
   methods: {
-
+    toggleSide: function(flashcard) {
+      flashcard.isFlipped = !flashcard.isFlipped;
+    },
+    addNewFC: function(event) {
+      event.preventDefault();
+      this.flashcards.push({
+        phrase: this.inputPhrase,
+        definition: this.inputDef,
+        isFlipped: false,
+      });
+    },
+    learnedCard: function(flashcard, index) {
+      flashcards.forEach((card) => {
+        if (card[index] === flashcard[index]) {
+          console.log('in here');
+          card.isLearned = !card.isLearned;
+        }
+      });
+    },
   },
 });
